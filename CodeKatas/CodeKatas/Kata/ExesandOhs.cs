194d1d3b67454726e5a555e50cea779b0a6e2ab8
Niels Vanden Eynde <niels.vandeneynde@hotmail.com>
2017-06-03 21:10:40 +0200

    ExesAndOhs

---
+++ CodeKatas/CodeKatas/Kata/ExesandOhs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeKatas.Kata
+{
+    public class ExesAndOhs: IKata
+    {
+        public bool XO(string input)
+        {
+            var chars = input.ToCharArray();
+            return chars.Count(c => Char.ToLower(c) == 'o') == chars.Count(c => Char.ToLower(c) == 'x');
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(XO("xo"));
+            Console.WriteLine(XO("xxxm"));
+            Console.WriteLine(XO("xxoo"));
+            Console.WriteLine(XO("xxMoo"));
+            Console.WriteLine(XO("xxvvoo"));
+        }
+    }
+}
